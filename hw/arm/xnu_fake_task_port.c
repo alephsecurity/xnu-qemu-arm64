c7e8d3954871dbb4750176f23fe7babf34afbcc9
Jonathan Afek <jonyafek@me.com>
2019-12-18 16:05:21 +0200

    xnu_fake_task_port: module to install fake kernel task port in special 4
    This module remaps the kernel task to another memory address,
    Creates a fake task port pointing to this new address
    and installs this port in host special port 4

---
+++ hw/arm/xnu_fake_task_port.c
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2019 Jonathan Afek <jonyafek@me.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "qemu-common.h"
+#include "hw/arm/arm.h"
+#include "exec/address-spaces.h"
+#include "hw/misc/unimp.h"
+#include "sysemu/sysemu.h"
+#include "qemu/error-report.h"
+#include "hw/platform-bus.h"
+#include "hw/arm/xnu_fake_task_port.h"
+
+static void copy_kernel_task_port(hwaddr kernel_port_new_addr,
+                                  hwaddr kernel_task_new_addr,
+                                  hwaddr kernel_port_orig_addr,
+                                  AddressSpace *as)
+{
+    uint8_t port_buffer[KERNEL_PORT_SIZE_16B92];
+
+    //read out the kernel task port
+    address_space_rw(as, kernel_port_orig_addr, MEMTXATTRS_UNSPECIFIED,
+                     &port_buffer[0], KERNEL_PORT_SIZE_16B92, 0);
+
+    //ovr write the kernel task pointer with the new copy
+    *(uint64_t *)&port_buffer[KERNEL_TASK_OFFSET_16B92] = \
+        ptov_static(kernel_task_new_addr);
+
+    //write the new kernel task port to memory
+    address_space_rw(as, kernel_port_new_addr,
+                     MEMTXATTRS_UNSPECIFIED, &port_buffer[0],
+                     KERNEL_PORT_SIZE_16B92, 1);
+}
+
+void setup_fake_task_port(void *opaque, hwaddr global_kernel_ptr)
+{
+    static bool done = false;
+    MemoryRegion *sysmem = NULL;
+    KernelTaskPortParams *ktpp = (KernelTaskPortParams *)opaque;
+    hwaddr task_port_ptr = 0;
+    hwaddr fake_port_paddr = ktpp->kalloc_paddr;
+    hwaddr fake_port_vaddr = ptov_static(fake_port_paddr);
+    hwaddr remap_task_paddr = fake_port_paddr + FAKE_PORT_ALLOC_SIZE;
+    hwaddr special_port_4_vaddr = KERNEL_REALHOST_SPECIAL_16B92 + \
+                                  (4 * sizeof(hwaddr));
+
+    //fprintf(stderr, "kernel_task_ptr_cb: starting\n");
+    //fprintf(stderr,
+    //        "n66_machine_init: kalloc_paddr: %016llX\n",
+    //        ptov_static(ktpp->kalloc_paddr));
+    if (done) {
+        //fprintf(stderr, "kernel_task_ptr_cb: already done\n");
+        return;
+    }
+
+    if ((0 == global_kernel_ptr) ||
+        (0 == (0xffffffff00000000 & global_kernel_ptr)) ||
+        (0 == (0x00000000ffffffff & global_kernel_ptr))) {
+        //fprintf(stderr, "kernel_task_ptr_cb: kernel global still uninited\n");
+        return;
+    }
+
+    //fprintf(stderr, "kernel_task_ptr_cb: kernl global vaddr: %016llX\n",
+    //        global_kernel_ptr);
+    //fprintf(stderr, "kernel_task_ptr_cb: kernl global paddr: %016llX\n",
+    //        vtop_mmu(global_kernel_ptr, ktpp->cs));
+
+    address_space_rw(ktpp->as, vtop_mmu(global_kernel_ptr, ktpp->cs) + \
+                     KERNEL_PORT_OFFSET_16B92,
+                     MEMTXATTRS_UNSPECIFIED, (uint8_t *)&task_port_ptr,
+                     sizeof(hwaddr), 0);
+
+    //fprintf(stderr, "kernel_task_ptr_cb: kernel port: %016llX\n",
+    //        task_port_ptr);
+
+    if (0 == task_port_ptr) {
+        //fprintf(stderr, "kernel_task_ptr_cb: kernel port still 0\n");
+        return;
+    }
+
+    //fprintf(stderr, "kernel_task_ptr_cb: kernel port is now available\n");
+    done = true;
+    sysmem = get_system_memory();
+
+    ktpp->remap.orig_pa = vtop_mmu(global_kernel_ptr,  ktpp->cs);
+    ktpp->remap.pa = remap_task_paddr;
+    ktpp->remap.orig_as = ktpp->as;
+    ktpp->remap.size = KERNEL_TASK_ALLOC_SIZE;
+
+    xnu_dev_remap_create(sysmem, &ktpp->remap, "kernel_task_remap_dev");
+
+    copy_kernel_task_port(fake_port_paddr, remap_task_paddr,
+                          vtop_mmu(task_port_ptr, ktpp->cs), ktpp->as);
+
+    address_space_rw(ktpp->as, vtop_mmu(special_port_4_vaddr, ktpp->cs),
+                     MEMTXATTRS_UNSPECIFIED, (uint8_t *)&fake_port_vaddr,
+                     sizeof(hwaddr), 1);
+}
---
+++ include/hw/arm/xnu_fake_task_port.h
@@ -0,0 +1,60 @@
+/* * iPhone 6s plus - n66 - S8000
+ *
+ * Copyright (c) 2019 Jonathan Afek <jonyafek@me.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef HW_ARM_XNU_FAKE_TASK_PORT_H
+#define HW_ARM_XNU_FAKE_TASK_PORT_H
+
+#include "qemu-common.h"
+#include "exec/hwaddr.h"
+#include "hw/boards.h"
+#include "hw/arm/arm.h"
+#include "hw/arm/xnu.h"
+#include "exec/memory.h"
+#include "cpu.h"
+#include "sysemu/kvm.h"
+#include "hw/arm/xnu_remap_dev.h"
+
+//TODO: support more versions
+#define KERNEL_TASK_PTR_16B92 (0xfffffff007602078)
+#define KERNEL_TASK_SIZE_16B92 (1440)
+#define KERNEL_PORT_SIZE_16B92 (168)
+#define KERNEL_PORT_OFFSET_16B92 (0xd8)
+#define KERNEL_TASK_OFFSET_16B92 (0x68)
+
+#define KERNEL_REALHOST_SPECIAL_16B92 (0xfffffff007607bc8)
+
+#define KERNEL_TASK_ALLOC_SIZE (0x2000)
+#define FAKE_PORT_ALLOC_SIZE (0x2000)
+#define KERNEL_STAIC_ALLOC_SIZE (KERNEL_TASK_ALLOC_SIZE + FAKE_PORT_ALLOC_SIZE)
+
+typedef struct {
+    CPUState *cs;
+    AddressSpace *as;
+    hwaddr kalloc_paddr;
+    RemapDev remap;
+} KernelTaskPortParams;
+
+void setup_fake_task_port(void *opaque, hwaddr global_kernel_ptr);
+
+#endif
+
